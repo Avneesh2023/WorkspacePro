40da59a9cea1f236d71f91647e8ff885c541278b
Avneesh2023 <avneeshpandey2313813.in>
2026-06-12 10:04:59 +0530

    Created client API service layer

---
+++ client/src/services/clientService.js
@@ -0,0 +1,49 @@
+import axios from 'axios';
+
+const API_URL = 'http://localhost:5000/api/clients';
+
+// Helper to construct headers with the current JWT token
+const getAuthHeaders = () => {
+  const token = localStorage.getItem('token');
+  return {
+    headers: {
+      Authorization: token ? `Bearer ${token}` : '',
+      'Content-Type': 'application/json',
+    },
+  };
+};
+
+const getClients = async () => {
+  const response = await axios.get(API_URL, getAuthHeaders());
+  return response.data;
+};
+
+const getClient = async (id) => {
+  const response = await axios.get(`${API_URL}/${id}`, getAuthHeaders());
+  return response.data;
+};
+
+const createClient = async (clientData) => {
+  const response = await axios.post(API_URL, clientData, getAuthHeaders());
+  return response.data;
+};
+
+const updateClient = async (id, clientData) => {
+  const response = await axios.put(`${API_URL}/${id}`, clientData, getAuthHeaders());
+  return response.data;
+};
+
+const deleteClient = async (id) => {
+  const response = await axios.delete(`${API_URL}/${id}`, getAuthHeaders());
+  return response.data;
+};
+
+const clientService = {
+  getClients,
+  getClient,
+  createClient,
+  updateClient,
+  deleteClient,
+};
+
+export default clientService;
