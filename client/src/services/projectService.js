2d27c11b3056186fd6e77761a45cb8543a9ad7e3
Avneesh2023 <avneeshpandey2313813.in>
2026-06-13 11:08:04 +0530

    Created project service layer

---
+++ client/src/services/projectService.js
@@ -0,0 +1,49 @@
+import axios from 'axios';
+
+const API_URL = 'http://localhost:5000/api/projects';
+
+// Helper to construct headers with the current JWT token
+const getAuthHeaders = () => {
+  const token = localStorage.getItem('token');
+  return {
+    headers: {
+      Authorization: token ? `Bearer ${token}` : '',
+      'Content-Type': 'application/json',
+    },
+  };
+};
+
+const getProjects = async () => {
+  const response = await axios.get(API_URL, getAuthHeaders());
+  return response.data;
+};
+
+const getProject = async (id) => {
+  const response = await axios.get(`${API_URL}/${id}`, getAuthHeaders());
+  return response.data;
+};
+
+const createProject = async (projectData) => {
+  const response = await axios.post(API_URL, projectData, getAuthHeaders());
+  return response.data;
+};
+
+const updateProject = async (id, projectData) => {
+  const response = await axios.put(`${API_URL}/${id}`, projectData, getAuthHeaders());
+  return response.data;
+};
+
+const deleteProject = async (id) => {
+  const response = await axios.delete(`${API_URL}/${id}`, getAuthHeaders());
+  return response.data;
+};
+
+const projectService = {
+  getProjects,
+  getProject,
+  createProject,
+  updateProject,
+  deleteProject,
+};
+
+export default projectService;
